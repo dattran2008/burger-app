c7d1c74bd6921df25bc71eda5f4a44cf6f9c7435
Dat Tran <datga2006@gmail.com>
2020-03-25 03:32:19 +0700

    - Update render burger ingredients dynamically

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -5,12 +5,28 @@ import BurgerIngredient from './ingredient/ingredient.js';
 
 const burger = (props) => {
     
+    //Transform object of ingredients passed from BurgerBuilder to Array
+    const temp = Object.keys(props.ingredients).map(igArr => {
+        //console.log(igArr);
+        /*create array which contains the amount of each ingredient in array
+        Ex: [ *salad[ null, null], cheese[null, null, null], ... ] */
+        
+        //create array for each ingredients with length by its amount
+        return [...Array(props.ingredients[igArr])].map((_, i) => {
+            //const test = [...Array(props.ingredients['bacon'])];
+            console.log([],igArr);
+            return (
+                <BurgerIngredient key={igArr + i} type={igArr} />
+                );
+            });
+        });
 
     return (
         <div className={classes.burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {/* <BurgerIngredient type='cheese' />
+            <BurgerIngredient type='meat' /> */}
+            {temp}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,16 +7,18 @@ class BurgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2,
+            bacon: 2,
+            cheese: 3,
+            meat: 5,
         }
     }
-
+    
+    
     render() {
+
         return(
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
